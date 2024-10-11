d117c0e1ec07e2ff5035d66f7512df3449adabcc
branxy <churkin.michael@gmail.com>
2024-10-11 14:51:30 +0300

    init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+
+// Load TSR devtools in dev mode
+const TanStackRouterDevtools =
+  import.meta.env.VITE_ENV === "development"
+    ? React.lazy(() =>
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      )
+    : () => null;
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <div className="flex h-screen flex-col md:flex-row">
+      <main className="h-full flex-grow overflow-y-auto overflow-x-hidden pb-24 pl-6 pr-3 pt-2 lg:pb-8">
+        <Outlet />
+      </main>
+      <TanStackRouterDevtools position="top-right" />
+    </div>
+  );
+}
